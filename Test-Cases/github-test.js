20ba6b0e163990ba793ede87fff5844fbc675de3
Ricardo-Cerqueira <ricardo.dp.cerqueira@gmail.com>
2024-02-26 23:05:44 +0000

    github test case assertions introduced

--- Test-Cases/github-test.js
+++ Test-Cases/github-test.js
@@ -9,6 +9,11 @@ describe('GitHub user profile Search', function() {
         try {
             await driver.manage().window().maximize();
             await driver.get('http://www.github.com');
+            await driver.sleep(1000);
+
+            //assert that is the github page
+            var newPage = await driver.getCurrentUrl();
+            expect(newPage).to.equal("https://github.com/")
 
             // Finding and clicking the search ID 
             await driver.findElement(By.className("search-input")).click();
@@ -34,18 +39,30 @@ describe('GitHub user profile Search', function() {
             // Select the Users option
             await driver.findElement(By.xpath('/html/body/div[1]/div[4]/main/react-app/div/div/div[1]/div/div/div[1]/div[2]/div/div/div/div/ul/li[1]/ul/li[6]/a/div/div/span[1]')).click();
             await driver.sleep(2000);
-            
+
             // assert that new page is correct
             var pageTitleUsers = await driver.getTitle();
             expect(pageTitleUsers).to.include('User search results')  // same reason as before
             // assert the URL is correct
             var usersUrl = await driver.getCurrentUrl();
             expect(usersUrl).to.equal("https://github.com/search?q=Ricardo-Cerqueira&type=users")
-
-
             await driver.sleep(2000);
-            // select Ricardo Cerqueira profile
-            await driver.findElement(By.xpath('/html/body/div[1]/div[4]/main/react-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/div[4]/div/div/div/div/div[1]/div[2]/h3/div/a[1]/span')).click();
+
+            // get Ricardo Cerqueira profile element
+            const hyperLink = await driver.findElement(By.xpath('/html/body/div[1]/div[4]/main/react-app/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/div[4]/div/div/div/div/div[1]/div[2]/h3/div/a[1]/span'));
+            // assert that the hyperlink refers to Ricardo Cerqueira
+            const hyperlinkText = await hyperLink.getText();
+            expect(hyperlinkText).to.equal('Ricardo Cerqueira');
+
+            // click on the profile element
+            await hyperLink.click();
+            await driver.sleep(2000);
+
+            // assert that the profile belongs to Ricardo Cerqueira
+            var userProfile = await driver.getTitle();
+            expect(userProfile).to.include("Ricardo-Cerqueira (Ricardo Cerqueira)")
+            var userUrl = await driver.getCurrentUrl();
+            expect(userUrl).to.equal("https://github.com/Ricardo-Cerqueira")
 
         } finally {
             await driver.quit();
